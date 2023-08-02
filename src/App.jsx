98e181ff315ae8ce77a0c52e8f837899d4697e17
Arvind <thakurarvindkr10@gmail.com>
2023-08-02 19:22:39 +0530

    format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function App() {
-  const show = useSelector(state=>state.ui.cartIsVisible);
+  const show = useSelector(state => state.ui.cartIsVisible);
   console.log(show)
   return (
     <Layout>
